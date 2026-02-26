deeab4c9734afcd85d2fd457e4cfdaddb85c4f0c
saibhavanavinnakota <saibhavana870@gmail.com>
2026-02-25 20:49:40 -0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter your name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+
+	if name == "" {
+		name = "Friend"
+	}
+
+	fmt.Println("Hello,", name, "👋")
+}
